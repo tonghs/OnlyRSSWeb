7223baac8edfb9f91793430ae76a9149ea911994
tonghuashuai <tonghuashuai@126.com>
2013-09-06 21:06:47 +0800

    修改数据库配置和模型

--- OnlyRSS/settings.py
+++ OnlyRSS/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'admin',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
--- OnlyRSSMain/models.py
+++ OnlyRSSMain/models.py
@@ -3,18 +3,18 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=30)
-    password = models.CharField()
+    password = models.CharField(max_length=200)
     name = models.CharField(max_length=30)
 
 
 class Feed(models.Model):
-    title = models.CharField()
-    url = models.CharField()
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=500)
 
 
 class Item(models.Model):
-    title = models.CharField()
-    url = models.CharField()
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=500)
     content = models.TextField()
     feed = models.ForeignKey(Feed)
     user = models.ForeignKey(User)
