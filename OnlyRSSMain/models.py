f85d56a6a076fa5e12859fa42071f3e64064aaed
tonghuashuai <tonghuashuai@126.com>
2013-09-06 20:41:05 +0800

    配置数据库，建模型

--- OnlyRSSMain/models.py
+++ OnlyRSSMain/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField()
+    name = models.CharField(max_length=30)
+
+
+class Feed(models.Model):
+    title = models.CharField()
+    url = models.CharField()
+
+
+class Item(models.Model):
+    title = models.CharField()
+    url = models.CharField()
+    content = models.TextField()
+    feed = models.ForeignKey(Feed)
+    user = models.ForeignKey(User)
