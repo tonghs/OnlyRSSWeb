5da33cfd58ed6b8e2f2544650f32fd265207bd95
tonghs <tonghuashuai@gmail.com>
2014-05-06 16:30:38 +0800

    fix

--- OnlyRSS/views.py
+++ OnlyRSS/views.py
@@ -94,15 +94,13 @@ def get_feed_content(request):
     list_temp = []
 
     for item in item_list:
-        dic_item = {'id': item.id, 'title': item.title, 'content': item.content, 'url': item.url,
+        content = item.content.replace('<a ', '<a target=_blank ')
+        dic_item = {'id': item.id, 'title': item.title, 'content': content, 'url': item.url,
                     'feed_title': item.feed.title,
                     'feed_url': item.feed.url}
         list_temp.append(dic_item)
 
-    #reg = r'''<img.*?src=\\["|']https?://(\w+?.feedsportal.com/|cdn.ifanr.cn/.*?RSSBanner.*?).+?\\["|']\s*?(>|/>|></img>)'''
     items_json = json.dumps(list_temp).replace('src', 'src_no')
-    #items_no_ad = re.sub(reg, '', items_json)
-    #items_no_ad = items_no_ad.replace('src', 'src_no')
 
     return HttpResponse(items_json)
 
