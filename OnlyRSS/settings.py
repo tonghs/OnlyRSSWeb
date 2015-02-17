839e23e4e8f25a2c22b7e609e8d301074ad33b3b
tonghs <tonghuashuai@gmail.com>
2015-02-17 21:11:07 +0800

    fix

--- OnlyRSS/settings.py
+++ OnlyRSS/settings.py
@@ -20,8 +20,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'onlyrss',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'admin',
+        'USER': 'rss',
+        'PASSWORD': 'rstfsgbcedh',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
