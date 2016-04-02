8c1ff4ce2d0204a59f3f0a6b2696cb1252216a32
tonghs <tonghuashuai@gmail.com>
2016-04-02 11:48:56 +0800

    migrate to flask mysql

--- OnlyRSS/settings.py
+++ OnlyRSS/settings.py
@@ -1,5 +1,5 @@
 # Django settings for OnlyRSS project.
-#coding=utf-8
+# coding=utf-8
 
 DEBUG = True
 
@@ -17,13 +17,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'onlyrss',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         # 'USER': 'root',
         # 'PASSWORD': 'admin',
-        'USER': 'rss',
-        'PASSWORD': 'rstfsgbcedh',
+        'USER': 'root',
+        'PASSWORD': 'tonghs',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
